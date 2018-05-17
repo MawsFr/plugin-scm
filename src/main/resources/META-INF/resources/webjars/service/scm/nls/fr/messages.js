7f7d70a7719abc0c68cd61e0d3e60ab19d0bdc55
frus68783 <frus68783@GFI520007.annuaire.groupe.local>
2018-05-17 11:33:11 +0200

    Adding repository and ldapgroups parameters + i18n

---
+++ src/main/resources/META-INF/resources/webjars/service/scm/nls/fr/messages.js
@@ -0,0 +1,7 @@
+define({
+	"service:scm" : "Source",
+	'service:scm:repository': 'Dépôt',
+	'service:scm:project': 'Projet',
+	'service:scm:ou': 'Organisation',
+	'service:scm:ldapgroups': 'Groupe(s) LDAP',
+});
--- src/main/resources/META-INF/resources/webjars/service/scm/nls/messages.js
+++ src/main/resources/META-INF/resources/webjars/service/scm/nls/messages.js
@@ -1,5 +1,10 @@
 define({
 	"root" : {
-		"service:scm" : "Source"
-	}
+		"service:scm" : "Source",
+		'service:scm:repository': 'Repository',
+		'service:scm:project': 'Project',
+		'service:scm:ou': 'Organization',
+		'service:scm:ldapgroups': 'LDAP groups',
+	},
+	fr: true
 });
